125021a38bb7bdee2a073b8dc82090fb2d4de38f
João Paulo <joaowais@gmail.com>
2023-11-04 15:44:14 -0300

    finalizada busca e página de itens

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/data-types';
+import { ProdutosService } from 'src/app/services/produtos.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  searchResult: undefined | Product[];
 
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private service: ProdutosService
+  ) {}
+
+  ngOnInit(): void {
+    let query = this.activeRoute.snapshot.paramMap.get('query');
+    query &&
+      this.service.searchProducts(query).subscribe((result) => {
+        this.searchResult = result;
+      });
+  }
 }
